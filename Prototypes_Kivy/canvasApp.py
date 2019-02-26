2c3de5a83cb2f23731892c6a3ce10d5598ed5615
KenoLeon <me@k3no.com>
2019-02-25 19:08:32 -0600

    canvasLayout

---
+++ Prototypes_Kivy/canvasApp.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+
+# Just a hard working canvas
+
+class canvasApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    canvasApp().run()
---
+++ Prototypes_Kivy/canvasLayoutApp.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+# Rigth Side Bar layout
+
+class canvasLayoutApp(App):
+    pass
+
+if __name__ == '__main__':
+    canvasLayoutApp().run()
