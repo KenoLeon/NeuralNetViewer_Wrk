aa9c7da952de38cc8ddfdf844f04ed25eb938fb8
KenoLeon <me@k3no.com>
2019-02-26 13:05:15 -0600

    canvasCall

---
+++ Prototypes_Kivy/canvasCallApp.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+
+# Just a hard working canvas
+
+class canvasCallApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    canvasCallApp().run()
---
+++ Prototypes_Kivy/wip001.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.config import Config
+
+
+# Window :
+Config.set('graphics', 'width', '1200')
+Config.set('graphics', 'height', '800')
+
+class wip001(App):
+    pass
+
+if __name__ == '__main__':
+    wip001().run()
